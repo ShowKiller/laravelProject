5a89831355f6ab95e22f64395799928571118564
ShowKiller <243356121@qq.com>
2019-02-12 22:48:59 +0800

    亚星

---
+++ money/app/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class IndexController extends Controller
+{
+    //index 方法
+    public function index()
+    {
+        $data = DB::table('user')->get();
+
+        return view('index');
+    }
+}
